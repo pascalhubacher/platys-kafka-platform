fc73803fa23f87392a401868da7aebf5b08d2719
Pascal Hubacher <pascal.hubacher@outlook.com>
2022-10-14 15:09:50 +0200

    add variable scripts

---
+++ kafka.cmd
@@ -0,0 +1,8 @@
+SET PUBLIC_IP=localhost
+echo "SET PUBLIC_IP=localhost"
+SET DOCKER_HOST_IP=localhost
+echo "SET DOCKER_HOST_IP=localhost"
+SET DATAPLATFORM_HOME=localhost
+echo "SET DATAPLATFORM_HOME=localhost"
+echo "docker-compose up -d"
+echo "docker-compose down"
---
+++ kafka.ps1
@@ -0,0 +1,9 @@
+$env:PUBLIC_IP = "localhost"
+$env:DOCKER_HOST_IP = "localhost"
+$env:DATAPLATFORM_HOME = "localhost"
+Write-Output '$env:PUBLIC_IP = "localhost" is set'
+Write-Output '$end:DOCKER_HOST_IP = "localhost" is set'
+Write-Output '$end:DATAPLATFORM_HOME = "localhost" is set'
+#Start-Process "C:\Users\pasca\AppData\Local\Programs\Rancher Desktop\Rancher Desktop.exe" -WorkingDirectory "C:\Users\pasca\AppData\Local\Programs\Rancher Desktop" -WindowStyle Hidden
+Write-Output 'docker-compose up -d'
+Write-Output 'docker-compose down'
